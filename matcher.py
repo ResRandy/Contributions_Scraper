d7907f16bc46177389c3068f2603de23f4e526bc
jacobjjo <jacobjjo@udel.edu>
2025-10-20 15:42:43 -0400

    Change matcher.py to iterate through all articles

--- matcher.py
+++ matcher.py
@@ -1,28 +1,34 @@
 import pymupdf
+import os
+
+files = [f for f in os.listdir("Articles") if os.path.isfile(os.path.join("Articles", f))]
 
 TESTING_ARTICLE = "Articles/Aquatic Conservation - 2012 - Caldwell - Revisiting two sympatric European seahorse species  apparent decline in the.pdf"
 
-doc = pymupdf.open(TESTING_ARTICLE)
-out = open("output.txt", "wb") # create a text output
-for page in doc: # iterate the document pages
-    text = page.get_text().encode("utf8") # get plain text (is in UTF-8)
-    out.write(text) # write text of page
-    out.write(bytes((12,))) # write page delimiter (form feed 0x0C)
-out.close()
+for f in files:
+    doc = pymupdf.open("Articles/" + f)
+    out = open("output.txt", "wb") # create a text output
+    for page in doc: # iterate the document pages
+        text = page.get_text().encode("utf8") # get plain text (is in UTF-8)
+        out.write(text) # write text of page
+        out.write(bytes((12,))) # write page delimiter (form feed 0x0C)
+    out.close()
 
-zoo_list = []
+    zoo_list = []
 
-# Transform the zoo list from leon's world to an array of names
-with open("zoo_aquarium_list.txt", 'r', encoding='utf8') as zoos:
-    zoo_list = zoos.readlines()
-    zoo_list = [zoo.replace("\n", "") for zoo in zoo_list]
+    # Transform the zoo list from leon's world to an array of names
+    with open("zoo_aquarium_list.txt", 'r', encoding='utf8') as zoos:
+        zoo_list = zoos.readlines()
+        zoo_list = [zoo.replace("\n", "") for zoo in zoo_list]
 
-# Parse the article txt and search for matches with every zoo name from leon
-file_name = "output.txt"
-with open(file_name, 'r', encoding='utf8') as file:
+    # Parse the article txt and search for matches with every zoo name from leon
+    file_name = "output.txt"
+    with open(file_name, 'r', encoding='utf8') as file:
 
-    # Joins every line of the article into one string, adds a space in between each line, removes all trailing whitepsace from each line, replaces all - with ""
-    article = " ".join(line.rstrip() for line in file).replace("-", "")
-    for zoo in zoo_list:
-        if zoo in article:  
-            print(f"Zoo/Aquarium detected: {zoo}")
+        # Joins every line of the article into one string, adds a space in between each line, removes all trailing whitepsace from each line, replaces all - with ""
+        article = " ".join(line.rstrip() for line in file).replace("-", "")
+        for zoo in zoo_list:
+            if zoo in article:
+                print(f)
+                print(f"Zoo/Aquarium detected: {zoo}")
+                

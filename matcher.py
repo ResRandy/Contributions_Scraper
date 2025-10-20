4f53870a043026585d8c8da1f86ee5d5482c25c8
jacobjjo <jacobjjo@udel.edu>
2025-10-20 10:04:52 -0400

    Cleaned zoo_aquarium_list, improved matcher.py POC

--- matcher.py
+++ matcher.py
@@ -1,7 +1,8 @@
-from pykml import parser
 import pymupdf
 
-doc = pymupdf.open("Articles/113_seahorse_JB.pdf")
+TESTING_ARTICLE = "Articles/Aquatic Conservation - 2012 - Caldwell - Revisiting two sympatric European seahorse species  apparent decline in the.pdf"
+
+doc = pymupdf.open(TESTING_ARTICLE)
 out = open("output.txt", "wb") # create a text output
 for page in doc: # iterate the document pages
     text = page.get_text().encode("utf8") # get plain text (is in UTF-8)
@@ -16,12 +17,12 @@ with open("zoo_aquarium_list.txt", 'r', encoding='utf8') as zoos:
     zoo_list = zoos.readlines()
     zoo_list = [zoo.replace("\n", "") for zoo in zoo_list]
 
-print(zoo_list)
 # Parse the article txt and search for matches with every zoo name from leon
 file_name = "output.txt"
 with open(file_name, 'r', encoding='utf8') as file:
-    article = file.read().replace('\n', '').replace("-", "")
 
+    # Joins every line of the article into one string, adds a space in between each line, removes all trailing whitepsace from each line, replaces all - with ""
+    article = " ".join(line.rstrip() for line in file).replace("-", "")
     for zoo in zoo_list:
-        if zoo in article:
-            print(zoo)
+        if zoo in article:  
+            print(f"Zoo/Aquarium detected: {zoo}")

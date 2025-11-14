3269a44baa45c110bb010f90430001ae40b5ba11
Nathan Wood <zestyalgaewafer@gmail.com>
2025-11-14 16:46:52 -0500

    Improved Matcher.py output

--- matcher.py
+++ matcher.py
@@ -18,31 +18,44 @@ def findContributions(files):
 
         # Transform the zoo list from leon's world to an array of names
         with open("zoo_aquarium_list.txt", 'r', encoding='utf8') as zoos:
-            zoo_list = zoos.readlines()
-            zoo_list = [zoo.replace("\n", "") for zoo in zoo_list]
+            zoo_list = [z.strip().lower() for z in zoos]
+            #zoo_list = [zoo.replace("\n", "") for zoo in zoo_list]
 
         # Parse the article txt and search for matches with every zoo name from leon
         file_name = "output.txt"
         with open(file_name, 'r', encoding='utf8') as file:
 
             # Joins every line of the article into one string, adds a space in between each line, removes all trailing whitepsace from each line, replaces all - with ""
-            article = " ".join(line.rstrip() for line in file).replace("-", "")
+            article = (" ".join(line.rstrip() for line in file).replace("-", "")).lower()
+            
             detected_zoos = []
             for zoo in zoo_list:
                 if zoo in article:
                     detected_zoos.append(zoo)
             
             detected_seahorse = False
-            if "Seahorse" or "seahorse" in article:
+            seahorse_mentions = 0
+            
+            if "seahorse" in article or "seahorses" in article:
                 detected_seahorse = True
-
-
-            if detected_zoos:
-                ending = f"\n {ending}\n{os.path.basename(f)}\n"
-                for zoo in detected_zoos:
-                    ending = (f"{ending} Zoo/Aquarium detected: {zoo}\n")
+                seahorse_mentions = article.count("seahorse") + article.count("seahorses")
                 if detected_seahorse:
-                    ending = ending + "The word seahorse is mentioned in article"
+                    print("The word seahorse is mentioned in article", seahorse_mentions, " times")
+                else: 
+                    print("The word seahorse is not mentioned in article")                
+                
+        ending += "File Name: " + os.path.basename(f) + "\n"
+        if detected_zoos:
+            for zoo in detected_zoos:
+                ending += f"Zoo/Aquarium detected: {zoo}\n"
+        else:
+            ending += "No Zoo/Aquarium detected\n"
+
+        if detected_seahorse:
+            ending += f"Seahorses mentioned {seahorse_mentions} times\n"
+        else:
+            ending += "Seahorses not mentioned\n"
+
         ending += "\n"
 
     return ending
@@ -66,28 +79,38 @@ def main():
 
         # Transform the zoo list from leon's world to an array of names
         with open("zoo_aquarium_list.txt", 'r', encoding='utf8') as zoos:
-            zoo_list = zoos.readlines()
-            zoo_list = [zoo.replace("\n", "") for zoo in zoo_list]
+            zoo_list = [z.strip().lower() for z in zoos]
+            #zoo_list = [zoo.replace("\n", "") for zoo in zoo_list] 
 
         # Parse the article txt and search for matches with every zoo name from leon
         file_name = "output.txt"
         with open(file_name, 'r', encoding='utf8') as file:
 
             # Joins every line of the article into one string, adds a space in between each line, removes all trailing whitepsace from each line, replaces all - with ""
-            article = " ".join(line.rstrip() for line in file).replace("-", "")
+            article = (" ".join(line.rstrip() for line in file).replace("-", ""))
+            
             detected_zoos = []
             for zoo in zoo_list:
                 if zoo in article:
                     detected_zoos.append(zoo)
             
             detected_seahorse = False
-            if "Seahorse" or "seahorse" in article:
+            
+            
+            
+            if "seahorse" in article or "seahorses" in article:
                 detected_seahorse = True
+                seahorse_mentions = article.count("seahorse") + article.count("seahorses")
+                if detected_seahorse:
+                    print("The word seahorse is mentioned in article", seahorse_mentions, " times")
+                else: 
+                    print("The word seahorse is not mentioned in article")                
 
 
             if detected_zoos:
                 print("\n" + f)
                 for zoo in detected_zoos:
                     print(f"Zoo/Aquarium detected: {zoo}")
-                if detected_seahorse:
-                    print("The word seahorse is mentioned in article")
+                else: print("No Zoo/Aquarium detected in", f)
+
+                
---
+++ tk.py
@@ -0,0 +1,81 @@
+import tkinter as tk
+from tkinter.ttk import *
+from tkinter import filedialog, messagebox
+import pymupdf
+from matcher import findContributions
+
+
+
+def select_open_selection():
+    new_window = tk.Toplevel(master)
+    new_window.geometry("900x600")
+    new_window.title("This is test")
+    label = tk.Label(new_window, text="This is a new window")
+    label.pack(pady=20)
+    entry_var = tk.StringVar()
+    entry = tk.Entry(new_window, textvariable=entry_var)
+    entry.pack()
+
+def open_file_dialog():
+    try:
+        file_path = filedialog.askopenfilenames(
+            title="Select a file",
+            filetypes=(("PDF files", "*.pdf"), ("All files", "*.*")),
+            multiple=True
+        )
+        # print(f"Selected file: {file_path}")
+
+        if not file_path:
+            print("No file selected.")
+            return
+        
+        # Open and read the PDF
+        text_content = findContributions(file_path)
+        # for file in file_path:
+        #     pdf_document = pymupdf.open(file)
+        
+        # # Extract text from all pages
+        #     for page in pdf_document:
+        #         text_content += page.get_text()
+        #     text_context += "\n\n\n"
+
+        text_area.config(state='normal')
+            
+        # Clear existing text and insert new content
+        text_area.delete(1.0, tk.END)
+        text_area.insert(tk.END, text_content)
+
+        text_area.config(state='disabled')
+        
+        # # Close the PDF
+        # pdf_document.close()
+
+    except FileNotFoundError:
+        messagebox.showerror("Error", "The selected file was not found.")
+    except UnicodeDecodeError:
+        messagebox.showerror("Error", "Unable to decode the selected file.")
+    except Exception as e:
+        messagebox.showerror("Error", f"\nAn unexpected error occurred:\n" + str(e))
+
+master = tk.Tk()
+master.geometry("400x200")
+master.title("Main window")
+Label(master, text="Click the button to select a file and the results shall appear in the box below").pack(pady=7)
+findFileButton = tk.Button(master, text="open file explorer", command=open_file_dialog).pack(pady=5)
+text_area = tk.Text(master, wrap='word', height=10)
+text_area.pack(pady=20,expand=True, fill='both')
+text_area.config(state='disabled')
+
+
+#create menue
+menubar = tk.Menu(master)
+filemenu = tk.Menu(menubar, tearoff=0)
+filemenu.add_command(label="Open", command=open_file_dialog)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=master.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+master.config(menu=menubar)
+
+
+master.mainloop()

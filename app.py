b33012182d1398256170cb08cea6dd330ecf856a
Nathan Wood <zestyalgaewafer@gmail.com>
2025-11-14 21:17:31 -0500

    Improved app GUI

--- app.py
+++ app.py
@@ -2,43 +2,30 @@ import tkinter as tk
 from tkinter.ttk import *
 from tkinter import filedialog, messagebox
 from matcher import findContributions
+import tkinter as tk
+from tkinter import filedialog, messagebox
 
 
-
-def select_open_selection():
-    new_window = tk.Toplevel(master)
-    new_window.geometry("900x600")
-    new_window.title("This is test")
-    label = tk.Label(new_window, text="This is a new window")
-    label.pack(pady=20)
-    entry_var = tk.StringVar()
-    entry = tk.Entry(new_window, textvariable=entry_var)
-    entry.pack()
 
 def open_file_dialog():
     try:
         file_path = filedialog.askopenfilenames(
-            title="Select a file",
+            title="Select PDF Files",
             filetypes=(("PDF files", "*.pdf"), ("All files", "*.*")),
             multiple=True
         )
-
         if not file_path:
             print(f"{file_path}, Is not a file.")
             return
         
         text_content = findContributions(file_path)
-        
-
         text_area.config(state='normal')
             
         # Clear existing text and insert new content
         text_area.delete(1.0, tk.END)
         text_area.insert(tk.END, text_content)
-
         text_area.config(state='disabled')
         
-
     except FileNotFoundError:
         messagebox.showerror("Error", "The selected file was not found.")
     except UnicodeDecodeError:
@@ -48,13 +35,56 @@ def open_file_dialog():
 
 
 master = tk.Tk()
-master.geometry("900x600")
-master.title("Main window")
-Label(master, text="Click the button to select a file and the results shall appear in the box below").pack(pady=7)
-findFileButton = tk.Button(master, text="open file explorer", command=open_file_dialog).pack(pady=5)
-text_area = tk.Text(master, wrap='word', height=10)
-text_area.pack(pady=20,expand=True, fill='both')
+
+# center window
+width = 900
+height = 600
+
+screen_width = master.winfo_screenwidth()
+screen_height = master.winfo_screenheight()
+
+x = int((screen_width / 2) - (width / 2))
+y = int((screen_height / 2) - (height / 2))
+
+master.geometry(f"{width}x{height}+{x}+{y}")
+master.title("Scientific Contributions Scraper")
+master.configure(bg= "#242431")
+
+
+top = tk.Frame(master, bg="#2F2F42")
+top.pack(fill="x")
+
+tk.Label(
+    top,
+    text="Please select the PDFs you want to scrape for scientific Contributions",
+    font=("Lucida Handwriting", 10, "bold"),
+    bg="#2F2F42",
+    fg="white"
+).pack(pady=5)
+
+findFileButton = tk.Button(
+    top,
+    text="open file explorer",
+    command=open_file_dialog,
+    font=("Lucida Handwriting", 14, "bold"),
+    bg="#242431",
+    fg="white",
+    bd=5,
+    relief="ridge"
+).pack(pady = 10)
+#findFileButton.pack(pady=5)
+
+text_area = tk.Text(
+    master,
+    wrap='word',
+    height=10,
+    bg = "#242424",
+    font=("Lucida Handwriting", 12),
+    fg="white"
+)
+text_area.pack(pady=20, expand=True, fill='both')
 text_area.config(state='disabled')
+
 
 
 # #create menue
